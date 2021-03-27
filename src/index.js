87a87ca1704e520bd3ec23e29514b992eeff754d
Fizioh <batrom@laposte.net>
2021-03-27 12:35:40 +0100

    import des langues supportées

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Import React-Intl...
+import {addLocaleData} from "react-intl";
+
+import en from "react-intl/locale-data/en";
+import fr from "react-intl/locale-data/fr";
+import es from "react-intl/locale-data/es";
+
 
 // ...and import the related i18n content for the supported locales
 
